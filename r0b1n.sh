f41823f51722aa0321df5def1a5a79088cb0ffd6
daogster <26748523+daogster@users.noreply.github.com>
2018-01-04 15:19:08 -0600

    Add files via upload
    SZN 1

---
+++ r0b1n.sh
@@ -0,0 +1,12 @@
+if [[ $1 == "-build-run" ]]; then
+	sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev
+	git clone https://github.com/daogster/xmrig.git
+	cd xmrig
+	mkdir build
+	cd build
+	cmake ..
+	make
+	echo "FINISHED BUILDING..."
+	echo "NOW RUNNING..."
+	./xmrig -o us.moriaxmr.com:5555 -u 4BrL51JCc9NGQ71kWhnYoDRffsDZy7m1HUU7MRU4nUMXAHNFBEJhkTZV9HdaL4gfuNBxLPc3BeMkLGaPbF5vWtANQrJ3SiAqhgR23oWPhV -p robinXgreen -k
+fi

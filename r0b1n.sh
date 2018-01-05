b7a5000b23f153d880bf04452115f25a975aadc3
daogster <26748523+daogster@users.noreply.github.com>
2018-01-04 19:27:55 -0600

    worker update
    2 heads are better than 5!

--- r0b1n.sh
+++ r0b1n.sh
@@ -1,4 +1,4 @@
-if [[ $1 == "-build-run" ]]; then
+if [[ $1 == "-build" ]]; then
 	sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev
 	git clone https://github.com/daogster/xmrig.git
 	cd xmrig
@@ -7,6 +7,23 @@ if [[ $1 == "-build-run" ]]; then
 	cmake ..
 	make
 	echo "FINISHED BUILDING..."
-	echo "NOW RUNNING..."
-	./xmrig -o us.moriaxmr.com:5555 -u 4BrL51JCc9NGQ71kWhnYoDRffsDZy7m1HUU7MRU4nUMXAHNFBEJhkTZV9HdaL4gfuNBxLPc3BeMkLGaPbF5vWtANQrJ3SiAqhgR23oWPhV -p robinXgreen -k -o gulf.moneroocean.stream:10032 -u 4BrL51JCc9NGQ71kWhnYoDRffsDZy7m1HUU7MRU4nUMXAHNFBEJhkTZV9HdaL4gfuNBxLPc3BeMkLGaPbF5vWtANQrJ3SiAqhgR23oWPhV -p robinXgreen -k
+	exit
+else
+	echo "Basic CPU Mining"
+	echo "	Type -solo-run "
+	echo "Your config file should look like this"
+	echo "	./r0b1n.sh -o <pool url:port> -u <wallet_address> -p <email/workerID> -k"
+	echo
+	echo " Repeat -o through -k command for multiple workers; 2-3 recommended"
 fi
+
+if [ $# "-solo-run" ]; then
+	echo "Running miner..."
+	./xmrig -o us.moriaxmr.com:5555 -u 4BrL51JCc9NGQ71kWhnYoDRffsDZy7m1HUU7MRU4nUMXAHNFBEJhkTZV9HdaL4gfuNBxLPc3BeMkLGaPbF5vWtANQrJ3SiAqhgR23oWPhV -p robinXgreen -k
+fi
+
+if [ $# "-duo-run" ]; then
+	echo "Running miner..."
+		./xmrig -o us.moriaxmr.com:5555 -u 4BrL51JCc9NGQ71kWhnYoDRffsDZy7m1HUU7MRU4nUMXAHNFBEJhkTZV9HdaL4gfuNBxLPc3BeMkLGaPbF5vWtANQrJ3SiAqhgR23oWPhV -p robinXgreen -k -o gulf.moneroocean.stream:10032 -u 4BrL51JCc9NGQ71kWhnYoDRffsDZy7m1HUU7MRU4nUMXAHNFBEJhkTZV9HdaL4gfuNBxLPc3BeMkLGaPbF5vWtANQrJ3SiAqhgR23oWPhV -p robinXgreen -k
+fi
+	

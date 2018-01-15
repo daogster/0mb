6bc9daf157520ab0e5504b0f30eec9770d78f659
daogster <26748523+daogster@users.noreply.github.com>
2018-01-15 15:25:12 -0600

    Delete r0b1n.sh

--- r0b1n.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-if [[ $1 == "--run" ]]; then
-	bash ./xmrig -o us.moriaxmr.com:5555 -u 4BrL51JCc9NGQ71kWhnYoDRffsDZy7m1HUU7MRU4nUMXAHNFBEJhkTZV9HdaL4gfuNBxLPc3BeMkLGaPbF5vWtANQrJ3SiAqhgR23oWPhV -p robinXgreen -k
-fi

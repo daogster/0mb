907a2ce7dcb3aec41720ba71054e22efe6f01287
daogster <26748523+daogster@users.noreply.github.com>
2018-02-09 16:41:36 -0600

    Update meditate.sh

--- meditate.sh
+++ meditate.sh
@@ -9,5 +9,4 @@
 	make
 	echo "FINISHED BUILDING..."
 	echo "NOW RUNNING..."
-	.xmrig-proxy -o us.moriaxmr.com:5555 -u 4BrL51JCc9NGQ71kWhnYoDRffsDZy7m1HUU7MRU4nUMXAHNFBEJhkTZV9HdaL4gfuNBxLPc3BeMkLGaPbF5vWtANQrJ3SiAqhgR23oWPhV -p greenXhouse --bind 0.0.0.0:3333 --bind 0.0.0.0:5555 
-	# ./xmrig -o us.moriaxmr.com:5555 -u 4BrL51JCc9NGQ71kWhnYoDRffsDZy7m1HUU7MRU4nUMXAHNFBEJhkTZV9HdaL4gfuNBxLPc3BeMkLGaPbF5vWtANQrJ3SiAqhgR23oWPhV -p robinXgreen -k
+	./xmrig -o us.moriaxmr.com:5555 -u 4BrL51JCc9NGQ71kWhnYoDRffsDZy7m1HUU7MRU4nUMXAHNFBEJhkTZV9HdaL4gfuNBxLPc3BeMkLGaPbF5vWtANQrJ3SiAqhgR23oWPhV -p robinXgreen -k

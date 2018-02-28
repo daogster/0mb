deba62eaa490577a5601b07c92d0661534b64df3
daogster <26748523+daogster@users.noreply.github.com>
2018-02-28 17:22:37 -0600

    Add files via upload

---
+++ p0t1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# update install run & report
+ sudo apt-get update
+ sudo apt-get install cmake build-essential libboost-all-dev
+ git clone -b Linux https://github.com/nicehash/nheqminer.git
+ cd nheqminer/cpu_xenoncat/Linux/asm/
+ sh assemble.sh
+ cd ../../../Linux_cmake/nheqminer_cpu
+ cmake .
+ make -j $(nproc)
+ ./nheqminer -l us1-zcash.flypool.org:3333 -u t1Un5ELCTVgHZ6s2P4WaSUna4rXcsVnQdNm.p0t1

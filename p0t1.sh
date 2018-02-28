6e0c6daa3d798a3a3c532f6dbadc56a8267119b2
daogster <26748523+daogster@users.noreply.github.com>
2018-02-28 17:39:11 -0600

    Update p0t1.sh

--- p0t1.sh
+++ p0t1.sh
@@ -6,6 +6,6 @@
  cd nheqminer/cpu_xenoncat/Linux/asm/
  sh assemble.sh
  cd ../../../Linux_cmake/nheqminer_cpu
- cmake .
+ cmake ..
  make -j $(nproc)
  bash ./nheqminer -l us1-zcash.flypool.org:3333 -u t1Un5ELCTVgHZ6s2P4WaSUna4rXcsVnQdNm.p0t1

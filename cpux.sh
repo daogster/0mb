7a85e2914b8b1042705f9a49001b7232cc6e6e12
daogster <26748523+daogster@users.noreply.github.com>
2018-05-20 21:03:02 -0500

    Update cpux.sh

--- cpux.sh
+++ cpux.sh
@@ -6,7 +6,7 @@ sudo apt-get install cmake build-essential libboost-all-dev
 mkdir /downloads
 cd /downloads
 git clone https://github.com/feeleep75/nheqminer.git
-heqminer/nheqminer
+nheqminer/nheqminer
 mkdir build
 cd build
 cmake ..

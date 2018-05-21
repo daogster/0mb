eadf6a0d3db53ebbdc81909c6b7844771da5e25f
daogster <26748523+daogster@users.noreply.github.com>
2018-05-20 20:49:53 -0500

    Update cpux.sh

--- cpux.sh
+++ cpux.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install git cmake build-essential libboost-all-dev
-git clone -b Linux https://github.com/nanopool/nheqminer.git
-cd nheqminer/cpu_xenoncat/Linux/asm/
-sh assemble.sh
-cd ../../../Linux_cmake/nheqminer_cpu
-cmake .
-make -j $(nproc)
-bash ./nheqminer -l zec-us-east1.nanopool.org:6666 -u t1bNUhf69B1ThBcrBBpDamBwHu34Zix58uR/robxg/hightechchi@gmail.com -t 6 -cd 0 1
+sudo apt-get -y install tmux
+sudo apt-get install cmake build-essential libboost-all-dev
+mkdir /downloads
+cd /downloads
+git clone https://github.com/feeleep75/nheqminer.git
+heqminer/nheqminer
+mkdir build
+cd build
+cmake ..
+make
